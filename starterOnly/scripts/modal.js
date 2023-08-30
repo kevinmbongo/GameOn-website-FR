5caaf61b09d9c95f272885ddf8199130c29d1ec4
kevinmbongo <mbongo.kevin.pro@gmail.com>
2023-08-30 22:35:57 +0200

    feat(Project): close modal and enhance desktop design

--- starterOnly/scripts/modal.js
+++ starterOnly/scripts/modal.js
@@ -11,11 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
+const closeBtn = document.querySelector(".close");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.classList.add("show");
 }
+// close modal form
+function closeModal() {
+  modalbg.classList.remove("show");
+}
+// close when click anywhere outside of the modal
+window.onclick = function (event) {
+  if (event.target == modalbg) {
+    modalbg.classList.remove("show");
+  }
+};
