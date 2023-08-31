28bbc9a59687b102af58e7b9003791e83eaf1316
kevinmbongo <mbongo.kevin.pro@gmail.com>
2023-09-01 01:16:07 +0200

    feat(validForm): implement all error messages

--- starterOnly/scripts/validForm.js
+++ starterOnly/scripts/validForm.js
@@ -1,31 +1,102 @@
+const firstMessage =
+  "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+const lastMessage =
+  "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+const emailMessage = "Veuillez entrer une adresse mail valide.";
+const dateMessage = "Vous devez entrer votre date de naissance.";
+const quantityMessage = "Vous devez entrer une réponse.";
+
 let form = document.querySelector("form");
 let first = document.getElementById("first");
+let last = document.getElementById("last");
+let email = document.getElementById("email");
+let date = document.getElementById("birthdate");
+let quantity = document.getElementById("quantity");
 
 // check the validity of value
-function validInputValue(balise) {
+function validInputValue(balise, message) {
   if (balise.value.length < 2) {
-    first.parentElement.setAttribute("data-error-visible", "true");
-    first.parentElement.setAttribute(
-      "data-error",
-      "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
-    );
+    balise.parentElement.setAttribute("data-error-visible", "true");
+    balise.parentElement.setAttribute("data-error", message);
   } else {
-    first.parentElement.removeAttribute("data-error-visible", "true");
-    first.parentElement.removeAttribute(
-      "data-error",
-      "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
-    );
+    balise.parentElement.removeAttribute("data-error-visible");
+    balise.parentElement.removeAttribute("data-error");
+  }
+}
+
+// check email validity
+function validEmail(balise, message) {
+  let emailRegExp = /[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+/;
+  if (emailRegExp.test(balise.value)) {
+    balise.parentElement.removeAttribute("data-error-visible");
+    balise.parentElement.removeAttribute("data-error");
+  } else {
+    balise.parentElement.setAttribute("data-error-visible", "true");
+    balise.parentElement.setAttribute("data-error", message);
+  }
+}
+
+// check date validity
+function validDate(date, message) {
+  let dateRegExp =
+    /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+
+  function reverseDate(dateStr) {
+    const parts = dateStr.split("-");
+    if (parts.length === 3) {
+      return parts.reverse().join("-");
+    }
+    return dateStr;
+  }
+  const dateReverse = reverseDate(date.value);
+
+  if (dateRegExp.test(dateReverse)) {
+    date.parentElement.removeAttribute("data-error-visible");
+    date.parentElement.removeAttribute("data-error");
+  } else {
+    date.parentElement.setAttribute("data-error-visible", "true");
+    date.parentElement.setAttribute("data-error", message);
+  }
+}
+
+function validQuantityValue(balise, message) {
+  if (!balise.value) {
+    balise.parentElement.setAttribute("data-error-visible", "true");
+    balise.parentElement.setAttribute("data-error", message);
+  } else {
+    balise.parentElement.removeAttribute("data-error-visible");
+    balise.parentElement.removeAttribute("data-error");
   }
 }
 
 // submit event
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  validInputValue(first);
-  console.log(first);
+  validInputValue(first, firstMessage);
+  validInputValue(last, lastMessage);
+  validEmail(email, emailMessage);
+  validDate(date, dateMessage);
+  validQuantityValue(quantity, quantityMessage);
+  console.log(quantity.value);
 });
 
 // check validity on change event
 first.addEventListener("change", () => {
-  validInputValue(first);
+  validInputValue(first, firstMessage);
+});
+
+last.addEventListener("change", () => {
+  validInputValue(last, lastMessage);
+});
+
+email.addEventListener("change", () => {
+  validEmail(email, emailMessage);
+});
+
+date.addEventListener("change", () => {
+  validDate(date, dateMessage);
+});
+
+quantity.addEventListener("change", () => {
+  validQuantityValue(quantity, quantityMessage);
 });
